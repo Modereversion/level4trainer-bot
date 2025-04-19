706185e0664b54449c6d134f53722fac61e00e2c
Modereversion <konstantin.khoteev@gmail.com>
2025-04-19 04:11:44 +0300

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,1 +1,15 @@
-# DB connection
+# database/db.py
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from config import DATABASE_URL
+
+engine = create_async_engine(DATABASE_URL, echo=False)
+SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+Base = declarative_base()
+
+
+async def create_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
