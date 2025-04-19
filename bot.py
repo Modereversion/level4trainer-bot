b531c4a6daab07aa41b3ea91c9c5342d7ba133be
Modereversion <konstantin.khoteev@gmail.com>
2025-04-19 04:34:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,20 @@
-import asyncio
-from aiogram import Bot, Dispatcher
-from config import settings
+from aiogram import executor
+from loader import dp
+from handlers import start, learning, training, emergencies, exam, settings, admin
 from database.db import init_db
-from handlers import start, training, learning, exam, emergencies, settings as user_settings, admin
-from keyboards.reply import main_menu
 
-bot = Bot(token=settings.BOT_TOKEN, parse_mode='HTML')
-dp = Dispatcher()
-
-dp.include_routers(
-    start.router,
-    training.router,
-    learning.router,
-    exam.router,
-    emergencies.router,
-    user_settings.router,
-    admin.router
-)
-
-async def main():
+async def on_startup(dp):
+    # Инициализация БД
     await init_db()
-    await bot.delete_webhook(drop_pending_updates=True)
-    await bot.set_my_commands([], scope=None)
-    await bot.set_chat_menu_button(menu_button=main_menu)
-    await dp.start_polling(bot)
 
-if __name__ == '__main__':
-    asyncio.run(main())
+# Регистрируем все хендлеры
+start.register_handlers(dp)
+learning.register_handlers(dp)
+training.register_handlers(dp)
+emergencies.register_handlers(dp)
+exam.register_handlers(dp)
+settings.register_handlers(dp)
+admin.register_handlers(dp)
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
