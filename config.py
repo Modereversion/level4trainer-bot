021e72a1f97cf399e0375c9b696464c643d4de04
Modereversion <konstantin.khoteev@gmail.com>
2025-04-19 04:25:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,10 @@
-# config.py
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
 DATABASE_URL = os.getenv("DATABASE_URL")
-
-# Настройки AI
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-MODEL_NAME = "gpt-4o"
+GPT_API_KEY = os.getenv("GPT_API_KEY")
+USE_GPT = True  # оставить True для использования ChatGPT 4o
