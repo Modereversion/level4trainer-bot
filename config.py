6887a453a218031ba6307715c61fa7d43c82da11
Modereversion <konstantin.khoteev@gmail.com>
2025-04-19 04:39:41 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,18 @@
-import os
+from dataclasses import dataclass
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-BOT_TOKEN       = os.getenv("BOT_TOKEN")
-OPENAI_API_KEY  = os.getenv("OPENAI_API_KEY")
-DATABASE_URL    = os.getenv("DATABASE_URL") or os.getenv("DB_URL")
+@dataclass
+class Config:
+    bot_token: str
+    openai_key: str
+    database_url: str
+
+def load_config() -> Config:
+    return Config(
+        bot_token=os.getenv("BOT_TOKEN"),
+        openai_key=os.getenv("OPENAI_API_KEY"),
+        database_url=os.getenv("DATABASE_URL"),
+    )
