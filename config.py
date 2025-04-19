086a1334bf238a9d6957c759f3760464ee114886
Modereversion <konstantin.khoteev@gmail.com>
2025-04-19 04:35:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
-DATABASE_URL = os.getenv("DATABASE_URL")
-GPT_API_KEY = os.getenv("GPT_API_KEY")
-USE_GPT = True  # оставить True для использования ChatGPT 4o
+BOT_TOKEN       = os.getenv("BOT_TOKEN")
+OPENAI_API_KEY  = os.getenv("OPENAI_API_KEY")
+DATABASE_URL    = os.getenv("DATABASE_URL") or os.getenv("DB_URL")
